bba9e62004e46dc82d0d1420a0bd7549581b2425
AlexOstin7 <lofk1031@yandex.ru>
2018-05-02 19:41:30 +0400

    office filter ok #1

---
+++ src/main/java/ru/bellintegrator/practice/view/OfficeFilterView.java
@@ -0,0 +1,71 @@
+package ru.bellintegrator.practice.view;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class OfficeView {
+    @ApiModelProperty(hidden = true)
+    public String id;
+
+    public String name;
+
+    public String address;
+
+    public Integer phone;
+
+    public Boolean isActive;
+
+    public Long orgId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean IsActive) {
+        isActive = isActive;
+    }
+
+    public Long getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(Long orgId) {
+        this.orgId = orgId;
+    }
+
+    @Override
+    public String toString() {
+        return "OfficeView{" + "id='" + id + '\'' + ", name='" + name + '\'' + ", address='" + address + '\'' + ", phone=" + phone + ", isActive=" + isActive + ", orgId=" + orgId + '}';
+    }
+}
